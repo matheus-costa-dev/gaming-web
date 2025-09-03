0537864cd2d47876d4830ef54f1cdf7eebec6356
Rafael Costa <raf.pc.dev@gmail.com>
2025-09-03 13:19:34 -0300

    colocando animação na navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Hero from "./components/Hero"
+import About from "./components/About"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <main 
-    className="relative min-h-screen w-screen overflow-x-hidde">
+    className="relative min-h-screen w-screen overflow-x-hidden text-white bg-blue-50">
+      <Navbar />
       <Hero />
+      <About />
 
     </main>
   )
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,18 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger)
 
 function Hero() {
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hoverIndex, setHoverIndex] = useState(2);
   const [loadedVideos, setLoadedVideos] = useState(0);
   const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const totalVideos = 4;
   const currentVideoRef = useRef<HTMLVideoElement | null>(null);
@@ -17,7 +21,7 @@ function Hero() {
   useGSAP(() => {
     if (hasClicked) {
       // Preparar o vídeo de expansão (cópia que vai expandir)
-      gsap.set("#expanding-video", { 
+      gsap.set("#expanding-video", {
         visibility: "visible",
         scale: 0.2, // Começa pequeno como o hover video
         zIndex: 45
@@ -48,9 +52,9 @@ function Hero() {
             duration: 0.5,
             ease: "power1.in"
           });
-          
+
           // Esconder o vídeo de expansão
-          gsap.set("#expanding-video", { 
+          gsap.set("#expanding-video", {
             visibility: "hidden",
             scale: 0.2
           });
@@ -61,6 +65,53 @@ function Hero() {
     dependencies: [currentIndex],
     revertOnUpdate: true,
   })
+
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 97%)",
+      borderRadius:"0% 0% 40% 10%",
+    });
+
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    })
+  })
+
+  useEffect(() => {
+  const videos: HTMLVideoElement[] = [];
+
+  for (let i = 1; i <= totalVideos; i++) {
+    const video = document.createElement("video");
+    video.src = getVideoSrc(i);
+    video.muted = true;
+    video.preload = "auto";
+    video.onloadeddata = () => {
+      setLoadedVideos(prev => prev + 1);
+    };
+    videos.push(video);
+  }
+
+  return () => {
+    // cleanup
+    videos.forEach(v => v.src = "");
+  };
+}, []);
+
+  useEffect(()=>{
+    console.log(loadedVideos, totalVideos)
+    if (loadedVideos >= totalVideos) {
+      console.log("entrou aqui")
+      setIsLoading(false);
+    }
+  }, [loadedVideos])
 
   function getVideoSrc(index: number) {
     return `videos/hero-${index}.mp4`;
@@ -77,77 +128,97 @@ function Hero() {
   }
 
   return (
-    <div
-      id="video-frame"
-      className="relative h-screen w-screen overflow-hidden bg-black">
-      {/* vídeo de fundo */}
-      <video
-        id="current-video"
-        ref={currentVideoRef}
-        src={getVideoSrc(currentIndex)}
-        className="absolute inset-0 w-full h-full object-cover"
-        autoPlay
-        muted
-        loop
-        onLoadedData={handleVideoLoad}
-      />
+    <div className="relative h-screen w-screen overflow-hidden bg-white">
+      
+      {isLoading && (
+        <div className="flex justify-center items-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
+      
+      {/* Container com clip-path para o vídeo */}
+      <div
+        id="video-frame"
+        className="absolute inset-0 w-full h-full bg-black"
+      >
+        {/* vídeo de fundo */}
+        <video
+          id="current-video"
+          ref={currentVideoRef}
+          src={getVideoSrc(currentIndex)}
+          className="absolute inset-0 w-full h-full object-cover"
+          autoPlay
+          muted
+          loop
+          onLoadedData={handleVideoLoad}
+        />
 
-      {/* vídeo central (hover) */}
-      <div className="absolute inset-0 flex justify-center items-center z-40">
+        {/* vídeo central (hover) */}
+        <div className="absolute inset-0 flex justify-center items-center z-40">
+          <div
+            className="cursor-pointer overflow-hidden rounded-lg scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            onClick={handleMiniVideoClick}
+          >
+            <video
+              id="next-video"
+              ref={nextVideoRef}
+              src={getVideoSrc(hoverIndex)}
+              className="w-64 h-64 object-cover rounded-lg"
+              loop
+              muted
+              autoPlay
+            />
+          </div>
+        </div>
+
+        {/* Vídeo de expansão - cobre a tela toda */}
         <div
-          className="cursor-pointer overflow-hidden rounded-lg scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
-          onClick={handleMiniVideoClick}
+          id="expanding-video"
+          className="absolute inset-0 w-full h-full flex justify-center items-center"
+          style={{
+            visibility: 'hidden',
+            transformOrigin: 'center center'
+          }}
         >
           <video
-            id="next-video"
-            ref={nextVideoRef}
             src={getVideoSrc(hoverIndex)}
-            className="w-64 h-64 object-cover rounded-lg"
+            className="w-full h-full object-cover"
             loop
             muted
             autoPlay
           />
         </div>
-      </div>
 
-      {/* Vídeo de expansão - cobre a tela toda */}
-      <div 
-        id="expanding-video"
-        className="absolute inset-0 w-full h-full flex justify-center items-center"
-        style={{ 
-          visibility: 'hidden',
-          transformOrigin: 'center center'
-        }}
-      >
-        <video
-          src={getVideoSrc(hoverIndex)}
-          className="w-full h-full object-cover"
-          loop
-          muted
-          autoPlay
-        />
-      </div>
-
-      {/* textos */}
-
-      <div className="absolute left-0 z-50 w-full">
-        <div className="mt-24 px-5 sm:px-10">
-          <h1 className="text-white font-bold text-5xl">Redefine</h1>
-          <p className="mb-5 max-w-md text-white">
-            Enter the metagame layer <br /> unleash the play economy
-          </p>
-          <Button
-            id="watch-trailer"
-            title="watch trailer"
-            leftIcon={<TiLocationArrow />}
-            containerClass="bg-yellow-300 flex-center gap-1"
-
-          />
+        {/* textos dentro do vídeo */}
+        <div className="absolute left-0 z-50 w-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="text-white font-bold text-5xl">Redefine</h1>
+            <p className="mb-5 max-w-md text-white">
+              Enter the metagame layer <br /> unleash the play economy
+            </p>
+            <Button
+              id="watch-trailer"
+              title="watch trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="bg-yellow-300 flex-center gap-1"
+            />
+          </div>
         </div>
       </div>
-      <h1 className="text-5xl absolute bottom-5 right-5">
-        Gaming
-      </h1>
+
+
+        <h1 className="mix-blend-exclusion bg-blend-difference absolute bottom-5 right-5 uppercase font-black sm:right-10 sm:text-7xl md:text-9xl lg:text-[12rem] z-[60] pointer-events-none">
+          Gaming
+        </h1>
+
+
+
+
+
     </div>
   );
 }
